27da5920ee9031aadded6cd8beda0f1bcf964b50
sarahpetak <45038414+sarahpetak@users.noreply.github.com>
2018-11-27 08:42:33 -0500

    Update team1.py

--- team1.py
+++ team1.py
@@ -6,9 +6,9 @@
 #     move: A function that returns 'c' or 'b'
 ####
 
-team_name = 'The name the team gives to itself' # Only 10 chars displayed.
-strategy_name = 'The name the team gives to this strategy'
-strategy_description = 'How does this strategy decide?'
+team_name = 'Team1' # Only 10 chars displayed.
+strategy_name = 'Collude until betrayed'
+strategy_description = 'Always collude with the opponent until the opponent betrays; then always betray'
     
 def move(my_history, their_history, my_score, their_score):
     ''' Arguments accepted: my_history, their_history are strings.
@@ -17,6 +17,7 @@ def move(my_history, their_history, my_score, their_score):
     Make my move.
     Returns 'c' or 'b'. 
     '''
+    
 
     # my_history: a string with one letter (c or b) per round that has been played with this opponent.
     # their_history: a string of the same length as history, possibly empty. 
