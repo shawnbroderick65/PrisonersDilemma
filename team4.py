719c8c48fa07cd02cdfbfa2ea2fc3f63a705b351
williampa <austinwilliam2@gmail.com>
2018-11-27 08:02:29 -0500

    Update team4.py

--- team4.py
+++ team4.py
@@ -2,6 +2,8 @@
 # Each team's file must define four tokens:
 #     team_name: a string
 #     strategy_name: a string
+
+
 #     strategy_description: a string
 #     move: A function that returns 'c' or 'b'
 ####
