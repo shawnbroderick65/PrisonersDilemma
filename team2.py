bfba660bf4f4037e661496e5c5bedb297a02bc84
shawnbroderick65 <45038263+shawnbroderick65@users.noreply.github.com>
2018-11-28 08:42:33 -0500

    Beta 1 Team 2
    First Stage of Strategy

--- team2.py
+++ team2.py
@@ -1,3 +1,4 @@
+import random
 ####
 # Each team's file must define four tokens:
 #     team_name: a string
@@ -7,8 +8,8 @@
 ####
 
 team_name = 'Zygomatics' # Only 10 chars displayed.
-strategy_name = 'The name the team gives to this strategy'
-strategy_description = 'How does this strategy decide?'
+strategy_name = 'Adapt and Change'
+strategy_description = ''
     
 def move(my_history, their_history, my_score, their_score):
     ''' Arguments accepted: my_history, their_history are strings.
@@ -17,7 +18,25 @@ def move(my_history, their_history, my_score, their_score):
     Make my move.
     Returns 'c' or 'b'. 
     '''
-
+    my_history()
+    their_history()
+    percentc = 0
+    percentb = 0 
+    for their_history in range(their_history[-1]):
+        if 'c' in their_history():
+            percentc =+ 1
+    for their_history in range(their_history[-1]):
+        if 'b' in their_history():
+            percentb =+ 1
+    if percentc == percentb:
+        return random.str('c','b')
+    else:
+       if percentc < percentb:
+           return 'b'
+       if percentb < percentc:
+           return 'c'  
+          
+ 
     # my_history: a string with one letter (c or b) per round that has been played with this opponent.
     # their_history: a string of the same length as history, possibly empty. 
     # The first round between these two players is my_history[0] and their_history[0].
@@ -25,9 +44,6 @@ def move(my_history, their_history, my_score, their_score):
     
     # Analyze my_history and their_history and/or my_score and their_score.
     # Decide whether to return 'c' or 'b'.
-    
-    return 'c'
-
     
 def test_move(my_history, their_history, my_score, their_score, result):
     '''calls move(my_history, their_history, my_score, their_score)
