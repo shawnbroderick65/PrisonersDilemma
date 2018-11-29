fc25711c7118ccebb2dbd0ee9ec807265fff63b1
shawnbroderick65 <45038263+shawnbroderick65@users.noreply.github.com>
2018-11-29 08:34:53 -0500

    Team 2 Basic Strategy

--- team2.py
+++ team2.py
@@ -1,3 +1,4 @@
+import random
 ####
 # Each team's file must define four tokens:
 #     team_name: a string
@@ -6,9 +7,9 @@
 #     move: A function that returns 'c' or 'b'
 ####
 
-team_name = 'The name the team gives to itself' # Only 10 chars displayed.
-strategy_name = 'The name the team gives to this strategy'
-strategy_description = 'How does this strategy decide?'
+team_name = 'Zygomatics' # Only 10 chars displayed.
+strategy_name = 'Apapt and Change'
+strategy_description = 'If they have more betrays then concludes, will betray. Ff they have more concludes then betrays will conclude'
     
 def move(my_history, their_history, my_score, their_score):
     ''' Arguments accepted: my_history, their_history are strings.
@@ -17,18 +18,23 @@ def move(my_history, their_history, my_score, their_score):
     Make my move.
     Returns 'c' or 'b'. 
     '''
-
-    # my_history: a string with one letter (c or b) per round that has been played with this opponent.
-    # their_history: a string of the same length as history, possibly empty. 
-    # The first round between these two players is my_history[0] and their_history[0].
-    # The most recent round is my_history[-1] and their_history[-1].
-    
-    # Analyze my_history and their_history and/or my_score and their_score.
-    # Decide whether to return 'c' or 'b'.
-    
-    return 'c'
-
-    
+    percentc = 0
+    percentb = 0
+    if len(their_history) == 0:
+        return 'c'
+    else:
+        for x in range(len(their_history)):
+            if 'c' == their_history[x]:
+                percentc += 1
+        for y in range(len(their_history)):
+            if 'b' == their_history[y]:
+                percentb += 1
+        if percentc < percentb:
+                        return 'b'
+        if percentb < percentc:
+                        return 'c'  
+          
+  
 def test_move(my_history, their_history, my_score, their_score, result):
     '''calls move(my_history, their_history, my_score, their_score)
     from this module. Prints error if return value != result.
